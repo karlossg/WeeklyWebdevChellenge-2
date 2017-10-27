1fce55f02b3767c978dc8224bd5138aece0a5373
Karol <karol.gratus@gmail.com>
2017-10-27 12:45:21 +0200

    scripts

---
+++ src/scripts.js
@@ -0,0 +1,12 @@
+// var homeBtn = document.getElementById("homeBtn");
+var aboutBtn = document.getElementById("aboutBtn");
+console.log(aboutBtn);
+// homeBtn.onclick = function() {
+// TweenLite.to(window, 2, {scrollTo:{y:"#header", offsetY:2700}});
+// }
+
+
+// aboutBtn.addEventListener('click',function() {
+//     console.log(aboutBtn);
+//     TweenLite.to(document, 1, {scrollTo:{y:"#about", offsetY:0}})
+// });
