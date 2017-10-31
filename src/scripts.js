2bee857e4c7dbd5921db0c7b197caf3f21d917e4
Karol <karol.gratus@gmail.com>
2017-10-31 13:40:07 +0100

    hamburger menu

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,17 @@
+// burger menu
+$(".nav__burger").on("click", function(){
+    $(".nav").toggleClass("nav--open");
+    $(".nav__menu-elem").toggleClass("nav__menu-elem--open");     
+    $(".nav__burger").toggleClass("nav__burger--opened");
+});
+      
 // smooth scrolling 
 $('a[href^="#"]').on('click', function (event) {
     
     var target = $(this.getAttribute('href'));
     if (target.length) {
         event.preventDefault();
-        $('html, body').stop().animate({
+        $("html, body").stop().animate({
             scrollTop: target.offset().top+50
         }, 1000);
     }
@@ -13,17 +20,21 @@ $('a[href^="#"]').on('click', function (event) {
 // menu hiding on scroll & active menu
 var prev = 0;
 var $window = $(window);
-var nav = $('.header__wrapper');
+var nav = $(".header__wrapper");
 
-$window.on('scroll', function(){
+$window.on("scroll", function(){
   var scrollTop = $window.scrollTop();
-  nav.toggleClass('hidden', scrollTop > prev);
+  nav.toggleClass("hidden", scrollTop > prev);
   prev = scrollTop;
   var scrollDistance = $window.scrollTop();
-  $('section').each(function(i) {
+  $("section").each(function(i) {
     if ($(this).position().top <= scrollDistance) {
-            $('ul li a').removeClass('nav__menu-link--active');
-            $('li a').eq(i).addClass('nav__menu-link--active');
+            $("ul li a").removeClass("nav__menu-link--active");
+            $(".nav").removeClass("nav--open");
+            $(".nav__menu-elem").removeClass("nav__menu-elem--open");
+            $(".nav__burger").removeClass("nav__burger--opened");
+            $("li a").eq(i).addClass("nav__menu-link--active");
+            
     }
 });
 
